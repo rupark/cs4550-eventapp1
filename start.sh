8c25c88e75a7bca4bae5560039d373ed10488bda
rupark <33532219+rupark@users.noreply.github.com>
2021-03-10 10:27:47 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
-
-# Could factor some of this out into an env.sh
-# to share with deploy.sh
 export MIX_ENV=prod
-export PORT=4801
-
-CFGD=$(readlink -f ~/.config/photo_blog)
-
-if [ ! -e "$CFGD/base" ]; then
-    echo "Need to deploy first"
-    exit 1
-fi
-
-SECRET_KEY_BASE=$(cat "$CFGD/base")
-export SECRET_KEY_BASE
+# Common port range for this is 4000-10,000
+# Valid port range for a user app to listen
+# on is something like 1025-32767
+export PORT=4794
+export SECRET_KEY_BASE=7u8saWUIpXDBYtk84fWv+63KG6dAloVcLPRShiPyDCQtpJkihKlAX0llKXXyg10p
+export DATABASE_URL=ecto://photo_blog:uzae7aiw1eeV@localhost/photo_blog_dev
+export NODEBIN=`pwd`/assets/node_modules/.bin
+export PATH="$PATH:$NODEBIN"
 
 _build/prod/rel/photo_blog/bin/photo_blog start
